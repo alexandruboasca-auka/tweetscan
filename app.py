ac7be77c40e970a4433e19fa51004ea3faef9cae
MarvoloGrim <al3x95x@gmail.com>
2015-05-08 20:29:02 +0300

    first working vers

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-import cherrypy
-
+import os, os.path
 import re
+import datetime
+import json
+import collections
+
+import cherrypy
 
 import tweepy
 import sqlite3
@@ -13,9 +17,31 @@ asecret = '0efgtCpv18aoXzcXzBs8YCejimYVC63XlZUYgk77YlySw'
 with sqlite3.connect('tweets.db', check_same_thread=False) as connection:
     auth = tweepy.OAuthHandler(ckey, csecret)
     auth.set_access_token(atoken, asecret)
-    #twitterStream = tweepy.Stream(auth, OutListener())
-    #twitterStream.filter(track=["rihanna"])
-        
+    
+class WordCount():
+    
+    def __init__(self, sterm):
+        self.sterm = sterm
+    
+    def to_dict(self):
+        prep_stat = (self.sterm,)
+        pattern = re.compile('[ ]\w{4,}')
+        cursor = connection.cursor()
+        for row in connection.execute('SELECT * FROM tweets WHERE sterm=?', prep_stat):
+            match_word = re.findall(pattern, row[1].decode('utf-8'))
+            for word in match_word:
+                if (word.encode('utf-8') == 'http' or word.encode('utf-8') == 'https'):
+                    continue
+                prep_stat_two = (self.sterm, word.encode('utf-8').strip().lower(),)
+                cursor.execute('SELECT * FROM terms WHERE sterm=? AND aterm=?', prep_stat_two)
+                data = cursor.fetchone()
+                if data is None:
+                    prep_stat_three = (self.sterm, word.encode('utf-8').strip().lower(),datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),)
+                    connection.execute('INSERT INTO terms (sterm, aterm, mentions, fmention) VALUES (?,?,1,?)', prep_stat_three)
+                else:
+                    connection.execute('UPDATE terms SET mentions = mentions + 1 WHERE sterm = ? AND aterm = ?', prep_stat_two)           
+        connection.commit()
+      
 class OutListener(tweepy.StreamListener):
             
     def __init__(self, api=None):
@@ -26,8 +52,8 @@ class OutListener(tweepy.StreamListener):
     	
     def on_status(self, status):
         if (self.num_tweets <= self.max_tweets):
-            to_insert = (self.sterm, status.text.encode('utf-8'),)
-            connection.execute('INSERT INTO tweets (sterm, tweet) VALUES (?,?)', to_insert)
+            to_insert = (self.sterm, status.text.encode('utf-8'), datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            connection.execute('INSERT INTO tweets (sterm, tweet, date) VALUES (?,?,?)', to_insert)
             self.num_tweets += 1
             return True
         else:
@@ -40,61 +66,40 @@ class OutListener(tweepy.StreamListener):
 
 class MainApp(object):
     @cherrypy.expose
-    def search(self, search_term):
-        listener = OutListener()
-        listener.sterm = search_term
-        listener.max_tweets = 50
-        twitterStream = tweepy.Stream(auth, listener)
-        twitterStream.filter(track=[search_term])
-        return '''
-    done
-    '''
+    @cherrypy.tools.json_out()
+    def display(self, search_term):
+        objects_list = []
+        prep_stat = (search_term,)
+        counter = 0
+        for row in connection.execute('SELECT * FROM terms WHERE sterm = ? ORDER BY mentions DESC', prep_stat):
+            if counter > 30:
+                break
+            d = collections.OrderedDict()
+            d['text'] = row[1].decode('utf-8')
+            d['weight'] = row[2]
+            objects_list.append(d)
+            counter += 1
+        j_data = json.dumps(objects_list)
+        return j_data
 
+    @cherrypy.expose
+    def result(self, search_term):
+        #listener = OutListener()
+        #listener.sterm = search_term
+        #listener.max_tweets = 50
+        #twitterStream = tweepy.Stream(auth, listener)
+        #twitterStream.filter(track=[search_term])
+        wordc = WordCount(search_term)
+        wordc.to_dict()
+        return open('result.html')
+        
     @cherrypy.expose
     def shutdown(self):
         cherrypy.engine.exit()
     
     @cherrypy.expose
     def index(self):
-        return '''
-		<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-</head>
-<body>
-
-    <div class="header_container"> 
-            <header class="main_header">TWords</header>
-    </div>
-
-    <div class="search_container"> 
-        <form method="get" action="search">
-           <i id="settings_cog" class="fa fa-cog fa-5x"></i>
-            <input type="text" name="search_term" class="search_input" autofocus>
-            <i id="enter_arrow" class="fa fa-arrow-right fa-5x"></i>
-        </form> 
-    </div>
-
-    <div id="sett_cont" class="settings_container">
-
-    </div>
-
-    <script>
-        $( "#settings_cog" ).click(function() {
-            $( "#sett_cont" ).slideToggle();  
-        });
-    </script>
-
-    <a id="shutdown"; href="./shutdown">Shutdown Server</a>
-</body>
-</html>
-		'''
+        return open('index.html')
 
 if __name__ == '__main__':
 	cherrypy.quickstart(MainApp(), '/', 'app.config')		
